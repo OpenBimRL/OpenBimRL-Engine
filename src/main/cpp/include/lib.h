fca1ef13ef1196202567a232d40dc6b23803fa46
Florian <Florian.Becker-f86@ruhr-uni-bochum.de>
2023-12-06 11:56:01 +0100

    restructured lib header

--- src/main/cpp/include/lib.h
+++ src/main/cpp/include/lib.h
@@ -1,33 +1,10 @@
 #ifndef LIB_H
 #define LIB_H
 
-#include <map>
-#include <stdint.h>
 #include <stdio.h>
 
-typedef struct
-{
-    int size;
-    void *content;
-} ReturnType;
-
-static std::map<uint32_t, ReturnType> returnTypes;
-
-extern "C"
-{
-    int sum(int n1, int n2);
-    typedef struct
-    {
-        int a;
-        int b;
-    } mystruct;
-}
+extern "C" int sum(int n1, int n2);
 
 bool init();
-
-inline bool registerType(uint32_t magicByte, ReturnType type)
-{
-    return returnTypes.emplace(magicByte, type).second;
-}
 
 #endif
