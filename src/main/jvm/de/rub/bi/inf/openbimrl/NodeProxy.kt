2f6fedf2ba96d926fd96b0bcb3ac18d1f33502ed
Florian Becker <info@florianbecker.eu>
2024-01-16 13:02:59 +0100

    changed NodeProxy and fixed issues with null

--- src/main/jvm/de/rub/bi/inf/openbimrl/NodeProxy.kt
+++ src/main/jvm/de/rub/bi/inf/openbimrl/NodeProxy.kt
@@ -1,144 +1,74 @@
-package de.rub.bi.inf.openbimrl;
+package de.rub.bi.inf.openbimrl
 
-import de.rub.bi.inf.openbimrl.engine.ifc.IIFCModel;
-import de.rub.bi.inf.openbimrl.functions.AbstractFunction;
-import de.rub.bi.inf.openbimrl.functions.FunctionFactory;
+import de.rub.bi.inf.openbimrl.engine.ifc.IIFCModel
+import de.rub.bi.inf.openbimrl.functions.FunctionFactory
 
-import java.util.*;
+class NodeProxy(val node: NodeType) {
 
-/**
- * A proxy element for precalculation nodes. Performes the calculation of a single node function
- * and passes the input and output nodes.
- *
- * @author Marcel Stepien, Andre Vonthron
- */
-public class NodeProxy {
+    private val inputEdges = let {
+        if (node.getInputs()?.getInput() == null)
+            return@let null
 
-    private final Optional<List<ArrayList<EdgeProxy>>> inputEdges;
-    private final Optional<List<ArrayList<EdgeProxy>>> outputEdges;
-    private final NodeType node;
-    private AbstractFunction function;
+        val size = node.getInputs().getInput().size
+        val map = HashMap<Int, MutableList<EdgeProxy>?>(size)
+        for (i in 0..size)
+            map[i] = null
 
-    public NodeProxy(NodeType node) {
-        this.node = node;
-        this.function = FunctionFactory.getFunction(this);
+        return@let map
+    }
+    private val outputEdges = let {
+        if (node.getOutputs()?.getOutput() == null)
+            return@let null
 
-        //initialize lists
-        if (node.getInputs() != null && node.getInputs().getInput() != null)
-            inputEdges = Optional.of(List.of(new ArrayList<>(node.getInputs().getInput().size())));
-        else inputEdges = Optional.empty();
-        if (node.getOutputs() != null && node.getOutputs().getOutput() != null)
-            outputEdges = Optional.of(List.of(new ArrayList<>(node.getOutputs().getOutput().size())));
-        else outputEdges = Optional.empty();
+        val size = node.getOutputs().getOutput().size
+        val map = HashMap<Int, MutableList<EdgeProxy>?>(size)
+        for (i in 0..size)
+            map[i] = null
+
+        return@let map
     }
 
-    public void setInputEdge(EdgeProxy edge, int pos) {
+    val function = let { FunctionFactory.getFunction(this) }!!
 
-        //TODO catch case if pos > array.length
+    private fun setEdge(edgeMap: MutableMap<Int, MutableList<EdgeProxy>?>?, edge: EdgeProxy, pos: Int) {
+        if (edgeMap.isNullOrEmpty() || edgeMap.size <= pos) return
 
-        // omit is present check cause Marcel didn't care either
-        ArrayList<EdgeProxy> inputsAtPos = inputEdges.get().get(pos);
-
-        if (inputsAtPos == null) {
-            inputsAtPos = new ArrayList<>();
-            inputEdges.get().set(pos, inputsAtPos);
+        edgeMap[pos] = let {
+            val edges = edgeMap[pos] ?: ArrayList()
+            edges.add(edge)
+            return@let edges
         }
-
-        inputsAtPos.add(edge);
     }
 
-    public void setOutputEdge(EdgeProxy edge, int pos) {
-
-        //TODO catch case if pos > array.length
-
-        // omit is present check cause Marcel didn't care either
-        ArrayList<EdgeProxy> outputsAtPos = outputEdges.get().get(pos);
-
-        if (outputsAtPos == null) {
-            outputsAtPos = new ArrayList<>();
-            outputEdges.get().set(pos, outputsAtPos);
-        }
-
-        outputsAtPos.add(edge);
-    }
-
-    public ArrayList<EdgeProxy> getInputEdges(int pos) {
-        if (inputEdges.isEmpty()) return null;
-        if (pos >= inputEdges.get().size()) return null; // out of scope
-
-        return inputEdges.get().get(pos);
-
-    }
-
-    public EdgeProxy getInputEdge(int pos) {
-        ArrayList<EdgeProxy> edges = getInputEdges(pos);
-        if (edges != null && !edges.isEmpty()) {
-            return edges.get(0);
-        }
-        return null;
-    }
-
-    public ArrayList<EdgeProxy> getOutputEdges() {
-        return outputEdges.map(
-                arrayLists -> arrayLists.stream()
-                        .filter(Objects::nonNull) // filter null values
-                        .reduce(new ArrayList<>(arrayLists.size()), (acc, val) -> {
-                            acc.addAll(val);
-                            return acc;
-                        })
-        ).orElseGet(() -> new ArrayList<>(0)); // return empty list
-    }
-
-    public ArrayList<EdgeProxy> getInputEdges() {
-        return inputEdges.map(
-                arrayLists -> arrayLists.stream()
-                        .filter(Objects::nonNull) // filter null values
-                        .reduce(new ArrayList<>(arrayLists.size()), (acc, val) -> {
-                            acc.addAll(val);
-                            return acc;
-                        })
-        ).orElseGet(() -> new ArrayList<>(0)); // return empty list
-    }
-
-    public ArrayList<EdgeProxy> getOutputEdges(int pos) {
-        if (pos < outputEdges.get().size())
-            return outputEdges.get().get(pos);
-        return null;
-    }
-
-    public EdgeProxy getOutputEdge(int pos) {
-        ArrayList<EdgeProxy> edges = getOutputEdges(pos);
-        if (edges != null && !edges.isEmpty()) {
-            return edges.get(0);
-        }
-        return null;
-    }
+    private fun getEdges(edgeMap: Map<Int, MutableList<EdgeProxy>?>?, pos: Int) = edgeMap?.get(pos)
 
 
-    public NodeType getNode() {
-        return node;
-    }
+    fun setInputEdge(edge: EdgeProxy, pos: Int) = setEdge(inputEdges, edge, pos)
+    fun setOutputEdge(edge: EdgeProxy, pos: Int) = setEdge(outputEdges, edge, pos)
 
-    public AbstractFunction getFunction() {
-        return function;
-    }
+    fun getInputEdges(pos: Int) = getEdges(inputEdges, pos)
+    fun getInputEdges() = inputEdges?.values?.reduce { acc, edgeProxies ->
+        acc?.addAll(edgeProxies ?: emptyList())
+        acc
+    } ?: emptyList()
 
-    public void setFunction(AbstractFunction function) {
-        this.function = function;
-    }
+    fun getOutputEdges(pos: Int) = getEdges(outputEdges, pos)
+    fun getOutputEdges() = outputEdges?.values?.reduce { acc, edgeProxies ->
+        acc?.addAll(edgeProxies ?: emptyList())
+        acc
+    } ?: emptyList()
 
-    public int getNumberOfOutputs() {
-        return outputEdges.get().size();
-    }
+    fun getInputEdge(pos: Int) = getInputEdges(pos)?.getOrNull(0)
+    fun getOutputEdge(pos: Int) = getOutputEdges(pos)?.getOrNull(0)
 
-    public void execute(IIFCModel ifcModel, NodeProxy nodeProxy) {
 
+    fun getNumberOfOutputs() = outputEdges?.size ?: 0
+
+    fun execute(ifcModel: IIFCModel?, nodeProxy: NodeProxy) {
         try {
-            function.execute(ifcModel);
-        } catch (Exception e) {
-            e.printStackTrace();
+            function.execute(ifcModel)
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-
     }
-
 }
