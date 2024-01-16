7f8df9c4390605723c6e9b8237692c77332a2e00
Florian Becker <info@florianbecker.eu>
2024-01-16 19:31:31 +0100

    fixed crashing when id is not available

--- src/main/cpp/functions/filterByGUID.cpp
+++ src/main/cpp/functions/filterByGUID.cpp
@@ -1,6 +1,6 @@
 #include <ifcparse/IfcSchema.h>
 
-#include "./include/functions/filterByGUID.hpp"
+#include "../include/functions/filterByGUID.hpp"
 
 void filterByGUID()
 {
@@ -11,7 +11,15 @@ void filterByGUID()
     auto guid = std::string(input); // create c++ string from char*
     IfcParse::IfcFile *file = OpenBIMRLEngine::getCurrentFile();
 
-    IfcUtil::IfcBaseClass *ptr = file->instance_by_guid(guid);
+    IfcUtil::IfcBaseClass *ptr = nullptr;
+
+    try
+    {
+        ptr = file->instance_by_guid(guid);
+    }
+    catch (const IfcParse::IfcException &)
+    {
+    }
 
     setOutputPointer(0, ptr);
 }
