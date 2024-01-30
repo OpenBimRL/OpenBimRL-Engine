648d6eea05f21d439e50844c288186dc9b9da211
Florian Becker <info@florianbecker.eu>
2024-01-30 16:21:07 +0100

    added const modifiers

--- src/main/cpp/utils/native_ifc_object_to_string.cpp
+++ src/main/cpp/utils/native_ifc_object_to_string.cpp
@@ -5,12 +5,12 @@
 /// @param ifcObject only works if GUID is at 0th position
 /// @param stringPointer pointer to free memory (idealy zeros)
 /// @param maxLen string maximum size
-bool ifc_object_to_string(JNA::Pointer ifcObject, JNA::Pointer stringPointer, std::size_t maxLen)
+bool ifc_object_to_string(const JNA::Pointer ifcObject, JNA::Pointer stringPointer, const std::size_t maxLen)
 {
     if (ifcObject == nullptr)
         return false;
 
-    auto data = static_cast<IfcUtil::IfcBaseClass *>(ifcObject)->data();
+    const auto data = static_cast<IfcUtil::IfcBaseClass *>(ifcObject)->data();
     std::stringstream ss;
 
     // remove quotes from string
@@ -18,8 +18,8 @@ bool ifc_object_to_string(JNA::Pointer ifcObject, JNA::Pointer stringPointer, st
     guid.erase(std::remove(guid.begin(), guid.end(), '\''), guid.end());
 
     ss << data.type()->name() << '#' << guid;
-    auto str = ss.str();
-    std::memcpy(stringPointer, str.c_str(), std::min(maxLen, str.length()));
+    const auto str = ss.str();
+    std::memcpy(stringPointer, str.c_str(), std::min(maxLen, str.size()));
 
     return true;
 }
