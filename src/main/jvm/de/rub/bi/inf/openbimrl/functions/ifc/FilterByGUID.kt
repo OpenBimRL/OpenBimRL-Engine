23b76b6eaf98329bb2c52bb58a45851daa2aaa93
Florian Becker <info@florianbecker.eu>
2024-02-05 15:40:59 +0100

    added back filter by guid

---
+++ src/main/jvm/de/rub/bi/inf/openbimrl/functions/ifc/FilterByGUID.kt
@@ -0,0 +1,21 @@
+package de.rub.bi.inf.openbimrl.functions.ifc
+
+import com.sun.jna.Pointer
+import de.rub.bi.inf.nativelib.IfcPointer
+import de.rub.bi.inf.openbimrl.NodeProxy
+import de.rub.bi.inf.openbimrl.functions.NativeFunction
+
+/**
+ * Filters an IFC model and retrieves the element of a certain GUID.
+ *
+ * @author Marcel Stepien (reworked by Florian Becker)
+ */
+class FilterByGUID(nodeProxy: NodeProxy?) : NativeFunction(nodeProxy) {
+    override fun executeNative() {
+        nativeLib.filterByGUID()
+    }
+
+    override fun handlePointerOutput(at: Int, pointer: Pointer) {
+        super.handlePointerOutput(at, IfcPointer(pointer))
+    }
+}
