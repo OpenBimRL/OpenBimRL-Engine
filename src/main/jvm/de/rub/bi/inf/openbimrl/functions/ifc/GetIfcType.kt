2dad333a9072b9e3f22dc1b8ae1eb01a9ae1244f
Florian Becker <info@florianbecker.eu>
2024-04-18 18:55:58 +0200

    added GetIfcType function

--- src/main/jvm/de/rub/bi/inf/openbimrl/functions/ifc/GetIfcType.kt
+++ src/main/jvm/de/rub/bi/inf/openbimrl/functions/ifc/GetIfcType.kt
@@ -1,49 +1,20 @@
-package de.rub.bi.inf.openbimrl.functions.ifc;
+package de.rub.bi.inf.openbimrl.functions.ifc
 
-import de.rub.bi.inf.openbimrl.NodeProxy;
-import de.rub.bi.inf.openbimrl.functions.AbstractFunction;
-
-import java.util.ArrayList;
-import java.util.Collection;
+import de.rub.bi.inf.nativelib.IfcPointer
+import de.rub.bi.inf.openbimrl.NodeProxy
+import de.rub.bi.inf.openbimrl.functions.AbstractFunction
+import kotlin.collections.AbstractCollection
 
 /**
- * Retrieves the class type of an element as String value.
+ * Retrieves the class type of element as String value.
  *
  * @author Marcel Stepien
  */
-public class GetIfcType extends AbstractFunction {
-
-    public GetIfcType(NodeProxy nodeProxy) {
-        super(nodeProxy);
-    }
-
-    @Override
-    public void execute() {
-
-        Collection<Object> objects = new ArrayList();
-        Object input0 = getInput(0);
-        if (input0 instanceof Collection<?>) {
-            objects = (Collection<Object>) input0;
-        } else {
-            objects.add(input0);
+class GetIfcType(nodeProxy: NodeProxy?) : AbstractFunction(nodeProxy) {
+    override fun execute() {
+        when (val input = getInput<Any>(0)) {
+            is AbstractCollection<*> -> setResult(0, input.filterIsInstance<IfcPointer>().map { it.type })
+            is IfcPointer -> setResult(0, input.type)
         }
-
-        ArrayList<Object> resultValues = new ArrayList<>();
-
-        for (Object o : objects) {
-
-//            if (o instanceof IIFCClass ifcObj) {
-//                resultValues.add(ifcObj.getClassName());
-//            }
-
-        }
-
-        if (resultValues.size() == 1)
-            setResult(0, resultValues.get(0));
-        else
-            setResult(0, resultValues);
-
     }
-
-
 }
