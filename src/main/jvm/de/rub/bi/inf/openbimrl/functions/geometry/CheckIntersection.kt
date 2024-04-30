0481bb419d34a702a3899b2bc47de87549d06be3
Florian Becker <info@florianbecker.eu>
2024-04-30 13:07:26 +0200

    added CheckIntersection

--- src/main/jvm/de/rub/bi/inf/openbimrl/functions/geometry/CheckIntersection.kt
+++ src/main/jvm/de/rub/bi/inf/openbimrl/functions/geometry/CheckIntersection.kt
@@ -2,14 +2,44 @@ package de.rub.bi.inf.openbimrl.functions.geometry
 
 import de.rub.bi.inf.openbimrl.NodeProxy
 import de.rub.bi.inf.openbimrl.functions.AbstractFunction
+import javax.media.j3d.BoundingBox
 
 /**
- * Checks if geometries of two RegionalBSPTree3D are intersecting.
+ * Checks if geometries of two BoundingBoxes are intersecting.
  *
  * @author Marcel Stepien (reworked by Florian Becker)
  */
 class CheckIntersection(nodeProxy: NodeProxy?) : AbstractFunction(nodeProxy) {
     override fun execute() {
-        // TODO redo
+        val input1 = getInput<Any>(0)
+        val input2 = getInput<Any>(1)
+
+        val result: Any? = when {
+            input1 is Collection<*> && input2 is Collection<*> -> input1.filterIsInstance<BoundingBox>().map {
+                intersectAny(it, input2.filterIsInstance<BoundingBox>())
+            }
+
+            // if input2 is not a collection
+            input1 is Collection<*> && input2 is BoundingBox -> intersectAny(
+                input2,
+                input1.filterIsInstance<BoundingBox>()
+            )
+            // if input1 is not a collection
+            input1 is BoundingBox && input2 is Collection<*> -> intersectAny(
+                input1,
+                input2.filterIsInstance<BoundingBox>()
+            )
+            // none are collections
+            input1 is BoundingBox && input2 is BoundingBox -> input1.intersect(input2)
+            else -> null
+        }
+
+        setResult(0, result)
     }
+
+    private fun intersectAny(boundingBox: BoundingBox, targets: Collection<BoundingBox>): List<Boolean> {
+        return targets.map { it.intersect(boundingBox) }
+    }
+
+
 }
