aa54da2c114afd45638f8c4bd25b9eb539e8ba8a
Florian Becker <info@florianbecker.eu>
2024-07-23 12:16:23 +0200

    fixed some issues with dijkstra

--- src/main/jvm/de/rub/bi/inf/extensions/BoundingBox.kt
+++ src/main/jvm/de/rub/bi/inf/extensions/BoundingBox.kt
@@ -3,17 +3,22 @@ package de.rub.bi.inf.extensions
 import java.awt.geom.Path2D
 import javax.media.j3d.BoundingBox
 import javax.vecmath.Point3d
+import javax.vecmath.Tuple2d
+
+fun BoundingBox.lower() = Point3d().apply { this@lower.getLower(this) }
+fun BoundingBox.upper() = Point3d().apply { this@upper.getUpper(this) }
+
 
 fun BoundingBox.toRect(): Path2D {
     val rect = Path2D.Double()
-    val lower = Point3d()
-    val upper = Point3d()
-    this.getLower(lower)
-    this.getUpper(upper)
+    val upper = upper()
+    val lower = lower()
     rect.moveTo(lower.x, lower.z)
     rect.lineTo(lower.x, upper.z)
     rect.lineTo(upper.x, upper.z)
     rect.lineTo(upper.x, lower.z)
-    rect.closePath()
+    //rect.closePath()
     return rect
 }
+
+fun BoundingBox.center(): Point3d = Point3d().apply { this.interpolate(this@center.lower(), this@center.upper(), .5) }
--- src/main/jvm/de/rub/bi/inf/openbimrl/functions/geometry/CalculateDijkstraSearch.kt
+++ src/main/jvm/de/rub/bi/inf/openbimrl/functions/geometry/CalculateDijkstraSearch.kt
@@ -1,6 +1,7 @@
 package de.rub.bi.inf.openbimrl.functions.geometry
 
 import arrow.core.Either
+import de.rub.bi.inf.extensions.center
 import de.rub.bi.inf.utils.math.lerp
 import de.rub.bi.inf.extensions.toPoint3d
 import de.rub.bi.inf.extensions.toRect
@@ -19,21 +20,22 @@ import io.github.offlinebrain.khexagon.math.pixelToHex
 import java.util.*
 import javax.media.j3d.BoundingBox
 import javax.media.j3d.BoundingSphere
+import javax.vecmath.Point3d
 
 class CalculateDijkstraSearch(nodeProxy: NodeProxy) : DisplayableFunction(nodeProxy) {
     /**
      * start: [IfcPointer], end: [IfcPointer], bounds: [BoundingBox], obstacles: [List], layout [Layout], maxDistance [Double]
      */
     override fun execute() {
-        val start = getInputAsCollection(0).filterIsInstance<IfcPointer>()[0]?.polygon?.value
-        val bounds = getInputAsCollection(1).filterIsInstance<BoundingBox>()[0]?.toRect()
+        val start = getInputAsCollection(0).filterIsInstance<Point3d>().getOrNull(0)
+        val bounds = getInputAsCollection(1).filterIsInstance<BoundingBox>().getOrNull(0)?.toRect()
         val obstacles = filterObstacles(getInputAsCollection(2))
         val layout = getInput<Layout>(3)
-        val maxDistance = getInput<Double?>(4) ?: 100.0
+        val maxDistance = getInput<Any?>(4)?.toString()?.toDouble() ?: 100.0
 
-        if (start?.isEmpty == true || bounds == null || layout == null) return
+        if (start == null || bounds == null || layout == null) return
         val startHexCoordinate =
-            pixelToHex(layout, start!!.get().bounds2D.let { Point(it.x.toFloat(), it.y.toFloat()) }).hexRound()
+            pixelToHex(layout, start.let { Point(it.x.toFloat(), it.z.toFloat()) }).hexRound()
 
         val path = dijkstra<HexCoordinates>(
             from = startHexCoordinate,
@@ -43,7 +45,7 @@ class CalculateDijkstraSearch(nodeProxy: NodeProxy) : DisplayableFunction(nodePr
         )
 
         if (logger.isEmpty) return
-        logger.get().logGraphicalOutput(this.nodeProxy!!.node.id, path.let {
+        logger.get().logGraphicalOutput(this.nodeProxy.node.id, path.let {
             it.keys.map { key ->
                 Pair(
                     BoundingSphere(hexToPixel(layout, key).toPoint3d(), 1.0),
