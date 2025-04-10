b43c5c5ea5811a70b8cb2510c6c65b053147ae89
Florian Becker <info@florianbecker.eu>
2025-04-10 11:56:51 +0200

    changed CalculateDijkstraSearch to new IO Schema

--- src/main/jvm/de/rub/bi/inf/openbimrl/functions/geometry/CalculateDijkstraSearch.kt
+++ src/main/jvm/de/rub/bi/inf/openbimrl/functions/geometry/CalculateDijkstraSearch.kt
@@ -12,11 +12,13 @@ import de.rub.bi.inf.extensions.upper
 import de.rub.bi.inf.nativelib.IfcPointer
 import de.rub.bi.inf.openbimrl.NodeProxy
 import de.rub.bi.inf.openbimrl.functions.DisplayableFunction
+import de.rub.bi.inf.openbimrl.functions.annotations.FunctionInput
 import de.rub.bi.inf.openbimrl.functions.annotations.OpenBIMRLFunction
+import de.rub.bi.inf.openbimrl.utils.InvalidFunctionInputException
 import de.rub.bi.inf.openbimrl.utils.addPaddingToObstacles
+import de.rub.bi.inf.openbimrl.utils.math.lerp
 import de.rub.bi.inf.openbimrl.utils.math.neighbors
 import de.rub.bi.inf.openbimrl.utils.pathfinding.*
-import de.rub.bi.inf.openbimrl.utils.math.lerp
 import io.github.offlinebrain.khexagon.coordinates.HexCoordinates
 import io.github.offlinebrain.khexagon.math.Layout
 import io.github.offlinebrain.khexagon.math.Point
@@ -24,28 +26,63 @@ import io.github.offlinebrain.khexagon.math.hexToPixel
 import io.github.offlinebrain.khexagon.math.pixelToHex
 import javax.media.j3d.BoundingBox
 import javax.media.j3d.BoundingSphere
-import kotlin.math.*
+import kotlin.math.max
+import kotlin.math.min
 
 @OpenBIMRLFunction(name = "calculateDistancesFromElement")
 class CalculateDijkstraSearch(nodeProxy: NodeProxy) : DisplayableFunction(nodeProxy) {
+
+    @FunctionInput(0, IfcPointer::class)
+    lateinit var start: Collection<IfcPointer>
+
+    @FunctionInput(1)
+    lateinit var buildingBoundingBox: BoundingBox
+
+    @FunctionInput(2, IfcPointer::class)
+    lateinit var obstacles: Collection<IfcPointer>
+
+    @FunctionInput(3, IfcPointer::class)
+    lateinit var passageWays: Collection<IfcPointer>
+
+    @FunctionInput(4)
+    lateinit var layout: Layout
+
+    @FunctionInput(5, nullable = true)
+    var obstaclePadding: String? = null
+
+    @FunctionInput(6, nullable = true)
+    var maxDistance: String? = null
+
     /**
      * start: [IfcPointer], bounds: [BoundingBox], obstacles: [List], passage ways [List], layout [Layout],
      * maxDistance (optional) [Double]
      */
     override fun execute() {
 
-        val start = geometryFromPointers(getInputAsCollection(0))
-        val bBox = getInputAsCollection(1).filterIsInstance<BoundingBox>().getOrNull(0)
-        val bounds = bBox?.toRect()
-        val passages = geometryFromPointers(getInputAsCollection(3))
-        val layout = getInput<Layout>(4)
-        val obstaclePadding = getInput<Any?>(5)?.toString()?.toDouble() ?: 0.0
-        val maxDistance = getInput<Any?>(6)?.toString()?.toDouble() ?: 100.0
+        val startGeometry = geometryFromPointers(start)
+        val bounds = buildingBoundingBox.toRect()
+        val passages = geometryFromPointers(passageWays)
+        val obstaclePaddingDouble = this.obstaclePadding.let {
+            try {
+                return@let it?.toDouble() ?: 0.0
+            } catch (e: NumberFormatException) {
+                throw InvalidFunctionInputException("Error in function ${this.javaClass.getAnnotation(OpenBIMRLFunction::class.java).name}! Could not convert $it to double.")
 
-        val obstacles = addPaddingToObstacles(geometryFromPointers(getInputAsCollection(2)), obstaclePadding)
+            }
+        }
+        val maxDistance = this.maxDistance.let {
+            try {
+                it?.toDouble() ?: 100.0
+            } catch (e: NumberFormatException) {
+                throw InvalidFunctionInputException("Error in function ${this.javaClass.getAnnotation(OpenBIMRLFunction::class.java).name}! Could not convert $it to double.")
+            }
 
-        if (start.isEmpty() || bounds == null || layout == null) return
-        val startHexCoordinates = start.map { point ->
+        }
+
+        val obstacles = addPaddingToObstacles(geometryFromPointers(this.obstacles), obstaclePaddingDouble)
+
+        if (startGeometry.isEmpty()) return
+        val startHexCoordinates = startGeometry.map { point ->
             pixelToHex(layout, point.bounds2D.let { Point(it.x.toFloat(), it.y.toFloat()) }).hexRound()
         }
 
@@ -73,8 +110,13 @@ class CalculateDijkstraSearch(nodeProxy: NodeProxy) : DisplayableFunction(nodePr
                         interpolation
                     ).toSRGB()).toHex()
                 Pair(
-                    BoundingSphere(hexToPixel(layout, key).toPoint3d(max(bBox.lower().y, bBox.upper().y)), .25),
-                    mapOf("color" to Either.Right(color))
+                    BoundingSphere(
+                        hexToPixel(layout, key).toPoint3d(
+                            max(
+                                buildingBoundingBox.lower().y, buildingBoundingBox.upper().y
+                            )
+                        ), .25
+                    ), mapOf("color" to Either.Right(color))
                 )
             }
         })
