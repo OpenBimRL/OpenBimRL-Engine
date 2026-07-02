475a08f17d13de30f4d3115ceb9f0e4677a9181e
Florian Becker <info@florianbecker.eu>
2026-07-02 16:11:18 +0000

    fixed CalculateBuildingBounds

--- src/main/jvm/de/rub/bi/inf/openbimrl/functions/geometry/CalculateBuildingBounds.kt
+++ src/main/jvm/de/rub/bi/inf/openbimrl/functions/geometry/CalculateBuildingBounds.kt
@@ -2,22 +2,37 @@ package de.rub.bi.inf.openbimrl.functions.geometry
 
 import de.rub.bi.inf.openbimrl.NodeProxy
 import de.rub.bi.inf.openbimrl.functions.NativeFunction
+import de.rub.bi.inf.openbimrl.functions.annotations.FunctionOutput
+import de.rub.bi.inf.openbimrl.functions.annotations.FunctionPort
 import de.rub.bi.inf.openbimrl.functions.annotations.OpenBIMRLFunction
 import de.rub.bi.inf.openbimrl.utils.boundingBoxFromMemory
 import java.util.*
+import javax.media.j3d.BoundingBox
+import javax.vecmath.Point3d
 
 /**
  * returns the Buildings bounding box
  * @author Florian Becker
  */
-@OpenBIMRLFunction
+@OpenBIMRLFunction(
+    description = "Returns the building's bounding box.",
+    outputs = [
+        FunctionPort(0, "Bounds", BoundingBox::class),
+        FunctionPort(1, "CenterPoints", Point3d::class),
+    ],
+)
 class CalculateBuildingBounds(nodeProxy: NodeProxy) : NativeFunction(nodeProxy) {
+    @FunctionOutput(0)
+    var bounds: BoundingBox? = null
+
+    @FunctionOutput(1)
+    var centerPoint: Point3d? = null
+
     override fun executeNative() = nativeLib.calculatingBuildingBounds()
 
-
     override fun handleMemory(memoryQueue: Queue<MemoryStructure>) {
-        val result = boundingBoxFromMemory(memoryQueue.remove())
-        setResult(0, result.second)
-        setResult(1, result.first)
+        val (center, bbox) = boundingBoxFromMemory(memoryQueue.remove())
+        bounds = bbox
+        centerPoint = center
     }
 }
